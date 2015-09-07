46eceb263574936066b89d5c5e411e28869734ff
Will Dages <wdages@gmail.com>
2015-09-07 17:52:54 -0400

    Basic structure and styling setup, and start to build up nav.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,17 @@
 // jshint devel:true
+
+$(function($){
+    // Keeps the two nav items' hover states in sync
+    $('.js-hover-trigger').each(function (i, e) {
+        var href = $(e).attr('href');
+
+        $(e).on('mouseover', function () {
+            $('.sitenav a[href="' + href + '"]').addClass('active');
+        });
+
+        $(e).on('mouseleave', function () {
+            $('.sitenav a[href="' + href + '"]').removeClass('active');
+        });
+
+    });
+});
